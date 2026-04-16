355940b6097a7066913be3d23b24626a9c2c8a84
Your Name <you@example.com>
2026-04-16 20:27:03 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -60,8 +60,7 @@ HashMap * createMap(long capacity) {
 
 void insertMap(HashMap * map, char * key, void * value) {
     int pos = hash(key, map->capacity);
-    while (//map->buckets[pos] != NULL) ||//
-        map->buckets[pos]->key != NULL){
+    while (map->buckets[pos] != NULL) && map->buckets[pos]->key != NULL){
         pos = (pos+1) % (map->capacity);
     }
     Pair* par= createPair(key, value);
