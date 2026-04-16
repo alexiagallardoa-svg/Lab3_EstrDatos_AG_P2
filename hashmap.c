f3211f96477ffe1687dedd9af660a4111f76cedb
Your Name <you@example.com>
2026-04-16 20:55:41 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -53,7 +53,7 @@ HashMap * createMap(long capacity) {
         //Pair** nuevoB = malloc(sizeof(Pair*));
         //(*nuevoB) = NULL;
         //(*nuevoB)->key= NULL;
-        Pair** nuevoB= (Pair**)createPair(NULL, 0);
+        Pair** nuevoB= (Pair**)createPair(NULL, NULL);
         nuevo->buckets[i]= *nuevoB;
     }
     return nuevo;
